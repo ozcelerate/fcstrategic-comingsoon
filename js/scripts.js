f1cceca671116efb056bc8aa123ab30ab8695efb
duanevt <duanevantwest@gmail.com>
2017-10-04 11:19:24 +1000

    modify for FC Strategic

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "12 july 2017 12:00:00", /** Enter new date here **/
+		date: "12 november 2017 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
